ea91d2dda79b38362d50fad3194b3e6736b63a53
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-10-03 14:59:21 +0200

    Add prettier and husky

--- server/server.js
+++ server/server.js
@@ -1,27 +1,26 @@
 import express from "express";
-import bodyParser from 'body-parser';
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(bodyParser.json());
 
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Welcome!'
-    });
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome!",
+  });
 });
 
-app.get('/login', (req, res) => {
-    res.json({
-        username: 'Hello'
-    });
+app.get("/login", (req, res) => {
+  res.json({
+    username: "Hello",
+  });
 });
 
-app.post('/login', (req, res) => {
-   res.sendStatus(401);
+app.post("/login", (req, res) => {
+  res.sendStatus(401);
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log('Server started at: ', server.address());
+  console.log("Server started at: ", server.address());
 });
