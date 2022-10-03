fc2d71b783f113449b0a43f02374eae2d2e6f8d3
Sebastian Nordby <sebastian.nordby.bjornstad@wlcom.no>
2022-10-03 19:30:37 +0200

    Fixed serving of client index.html with express when route is not api

--- server/server.js
+++ server/server.js
@@ -17,8 +17,12 @@ app.post("/api/login", (req, res) => {
   res.sendStatus(401);
 });
 
-app.use((req, res) => {
-  res.sendFile(path.resolve("../client/dist/index.html"));
+app.use((req, res, next) => {
+  if (!req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
 });
 
 const server = app.listen(process.env.PORT || 3000, () => {
